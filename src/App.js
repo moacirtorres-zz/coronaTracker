b01c0994698d59c233312e5e528d33e1e196cc51
moacirtorres <moacirtorress@gmail.com>
2020-04-26 04:51:27 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+import { fetchData } from './api'
+
+class App extends React.Component  {
+
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData()
+    this.setState({ data: fetchedData })
+  }
+
+  render() {
+  return (
+    <div className={styles.container}>
+      <Cards data={this.state.data} />
+      <CountryPicker />
+      <Chart />
+    </div>
+    );
+  }
+}
+
+export default App;
